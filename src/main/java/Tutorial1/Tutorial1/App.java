80b7ff9642aad97f976b9ceb5c3bcb8f3b9a4578
unknown <prafpawar11@gmail.com>
2022-12-17 20:32:59 +0530

    new framework

---
+++ src/main/java/Tutorial1/Tutorial1/App.java
@@ -0,0 +1,13 @@
+package Tutorial1.Tutorial1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TestLayer/LoginPageTest.java
@@ -0,0 +1,12 @@
+package TestLayer;
+
+public class LoginPageTest {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Login page Test cases");
+
+	}
+
+}
